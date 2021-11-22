2b3910f97b9ac2c72e541cb846d72c09e17506d5
Ashlyn <ashlyn.barry1@gmail.com>
2021-11-22 23:19:09 +0000

    Created struct for token metadata

--- contracts/VolcanoToken.sol
+++ contracts/VolcanoToken.sol
@@ -2,9 +2,19 @@
 
 pragma solidity ^0.8.0;
 
+// import ERC721 and Ownable contracts from OpenZeppelin library
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VolcanoToken is ERC721, Ownable {
-    
+contract VolcanoToken is ERC721("VolcanoToken", "VOL"), Ownable {
+
+    uint256 tokenId;
+
+    struct Token {
+        uint256 timestamp;
+        uint256 tokenId;
+        string tokenURI;
+    }
+
+    constructor() {}
 }
