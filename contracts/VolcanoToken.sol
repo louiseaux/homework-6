10e22231ff6956a5f3709b55f4edd302104ac109
Ashlyn <ashlyn.barry1@gmail.com>
2021-11-22 23:04:44 +0000

    Created VolcanoToken contract

---
+++ contracts/VolcanoToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VolcanoToken is ERC721, Ownable {
+    
+}
